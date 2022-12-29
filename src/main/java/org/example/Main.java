520e37ecfd4460e72985aa6f37496e37f2c5aa4a
flobangs <flobangs.korea@gmail.com>
2022-12-29 17:13:01 +0900

    Usege Boot
    부트 사용 나오지 않음

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,22 @@
 package org.example;
 
 import logic.BubbleSort;
+import logic.JavaSort;
+import logic.Sort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import service.SortService;
 
+import java.io.ObjectInputFilter;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> bs = new BubbleSort<>();
 
-        System.out.println("result: " + bs.sort(Arrays.asList(args)));
+        ApplicationContext context = new AnnotationConfigApplicationContext(ObjectInputFilter.Config.class);
+        SortService sortService = context.getBean(SortService.class);
+
+        System.out.println("start");
+        System.out.println("result: " + sortService.doSort(Arrays.asList(args)));
     }
 }
